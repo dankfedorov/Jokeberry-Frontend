92205c8c860a12e57664ea0ea19287b14938b041
Dany Fedorov <dank.fedorov@gmail.com>
2024-12-24 17:00:26 +0300

    add video

--- src/pages/VideoBlog.js
+++ src/pages/VideoBlog.js
@@ -7,31 +7,20 @@ function VidePage() {
   return (
     <>
 
-       <section id="intro" className="text-white text-center video-blog">
-        <div className="container container__video  ">
-        <div className="row d-flex align-items-center justify-content-between">
-
-          <div className="col-md-6">
-          <h2 className="mb-2 h2__video">Промо блок</h2>
+      <section id="part2" className="text-white text-center video-blog ">
+        <div className="container container__video">
+          <div className="row d-flex align-items-center justify-content-between ">
+          <h2 className="mb-2 h2__video">Интро.</h2>
              <VideoStart/>        
-          </div>
-
-          <div className="col-md-6">
-          <h2 className="mb-2 h2__video">Промо блок</h2>
-             <VideoStart/>        
-          </div>
-          
           </div>
         </div>
       </section>
     
-       <section id="part1" className="text-white text-center video-blog ">
+      <section id="part2" className="text-white text-center video-blog ">
         <div className="container container__video">
           <div className="row d-flex align-items-center justify-content-between ">
-          <h2 className="mb-2 h2__video">1.DRAW.IO Создание архитектуры проекта.</h2>
-          <div className='container container__video'>
-          <iframe width="100%" height="580" src="https://rutube.ru/video/f1e2bd72fcb35fe0c3651e147274d7df/?r=wd" frameBorder="0" allow="clipboard-write; autoplay" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
-         </div>      
+          <h2 className="mb-2 h2__video">1.Draw Создание архитектуры сайта.</h2>
+          <iframe width="50%" height="550" src="https://rutube.ru/play/embed/f1e2bd72fcb35fe0c3651e147274d7df/" frameBorder="0" allow="clipboard-write; autoplay" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
           </div>
         </div>
       </section>
