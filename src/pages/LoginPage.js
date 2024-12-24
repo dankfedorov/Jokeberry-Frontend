e1b6b474cf245a93812c12ce918372a049f08661
Dany Fedorov <dank.fedorov@gmail.com>
2024-12-24 12:57:50 +0300

    add
    add https

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -19,8 +19,8 @@ const MyForm = () => {
 
     try {
       
-      const response = await fetch("http://localhost:5000/api/login", {
-      // const response = await fetch("https://188.130.154.26:5000/api/login", {
+      // const response = await fetch("http://localhost:5000/api/login", {
+      const response = await fetch("https://188.130.154.26:5000/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
