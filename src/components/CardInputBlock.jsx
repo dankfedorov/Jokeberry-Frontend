cb6c586606fef93cb57b9edb97210998e65719f9
Kirill <kir_fedorov@mail.ru>
2025-02-12 07:45:31 +0300

    changed color left slider

--- src/components/CardInputBlock.jsx
+++ src/components/CardInputBlock.jsx
@@ -54,13 +54,13 @@ const CardInputBlock = () => {
   return (
     <div className="container__cardinputblock">
     <div className="table-responsive small ">
-      <p className="m-3 text-small12">Таблица для заполнения</p>
+      <p className="text__black">Таблица для заполнения</p>
       <table class="table table-striped table-sm text-small12">
         <thead>
           <tr>
-            <th scope="col" className="col-md-1">№</th>
-            <th scope="col" className="col-md-3">Наименование</th>
-            <th scope="col" className="col-md-1">Image</th>
+            <th scope="col" className="col-md-1 ">№</th>
+            <th scope="col" className="col-md-3 ">Наименование</th>
+            <th scope="col" className="col-md-1 ">Image</th>
             <th scope="col" className="col-md-1">Pricein</th>
             <th scope="col" className="col-md-1">Priceout</th>
             <th scope="col" className="col-md-1 ">Status</th>
@@ -68,7 +68,7 @@ const CardInputBlock = () => {
         </thead>
         <tbody>
   
-          <tr >
+          <tr className="">
             <td className="col-md-1"><input type="text" name="id_item" className="" placeholder="id_item" value={ItemData.id_item} onChange={handleChange}/></td>
             <td className="col-md-3"><input type="text" name="description" className="" placeholder="название" value={ItemData.description} onChange={handleChange}/></td>
             <td className="col-md-1"><input type="text" name="photos" className="" placeholder="фотография" value={ItemData.photos} onChange={handleChange}/></td>
@@ -81,8 +81,8 @@ const CardInputBlock = () => {
         
       </table>
       <div className="btn-block p-1">
-        <button id="submitBtn" className="btn btn-success " type="submit">Добавить запись</button> 
-        <button id="delBtn" className="btn btn-danger" type="del">Удалить запись</button> 
+        {/* <button id="submitBtn" className="btn btn-success " type="submit">Добавить запись</button>  */}
+        {/* <button id="delBtn" className="btn btn-danger" type="del">Удалить запись</button>  */}
         </div>
     </div>
     </div>
