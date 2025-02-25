d886732320730bda50fd8eec44ce8fdbf8557e60
kir_fedorov <kir_fedorov@mail.ru>
2025-02-25 11:57:50 +0300

    changed land page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,13 @@ import "../assets/styles/header.css";
 import Logo from "./Logo";
 import Nav from "./Nav";
 import TopLinks from "./TopLinks";
-import TelegramLink from "./TelegramLink";
+
 // import PartnerLinks from "./PartnerLinks";
 
 
 function Header() {
   return (
-    <header id="header" className="header p-3">
+    <header id="header" className="header">
       <div className="container container__header ">
       
         <Logo />
