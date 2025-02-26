29cfe3c5994cc19240a7882c9ce79625fbb1d5b6
kir_fedorov <kir_fedorov@mail.ru>
2025-02-26 14:51:13 +0300

    try to add floating backg

--- src/components/TopLinks.jsx
+++ src/components/TopLinks.jsx
@@ -1,42 +1,44 @@
 import React from "react";
 // import menu from "../assets/images/menu.png";
- // меню сверху
+// меню сверху
 
 function TopLinks() {
   return (
+
+    
+      <ul className="d-felx toplink__group ms-auto">
+        <li className="toplink__item ">
+          <a href="/" className="toplink-link dropdown">
+            Главная
+          </a>
+          {/*  */}
+          
+          {/*  */}
+        </li>
+
+        <li className="toplink__item ">
+          <a href="/present" className="toplink-link">
+            О Проекте
+          </a>
+        </li>
+
+        <li className="toplink__item ">
+          <a href="#" className="toplink-link">
+            Проекты и идеи
+          </a>
+        </li>
+        <li className="toplink__item ">
+          <a href="#" className="toplink-link">
+            Новости
+          </a>
+        </li>
+        <li className="toplink__item ">
+          <a href="#" className="toplink-link">
+            Контакты
+          </a>
+        </li>
+      </ul>
    
-
-    <ul className="d-felx toplink__group ms-auto">
-      <li className="toplink__item ">
-        <a href="/" className="toplink-link">
-          Главная
-        </a>
-      </li>
-        
-      <li className="toplink__item ">
-        <a href="/present" className="toplink-link">
-          О Проекте
-        </a>
-      </li>
-
-      <li className="toplink__item ">
-        <a href="#" className="toplink-link">
-        Проекты и идеи
-        </a>
-      </li>
-      <li className="toplink__item ">
-        <a href="#" className="toplink-link">
-        Новости
-        </a>
-      </li>
-      <li className="toplink__item ">
-        <a href="#" className="toplink-link">
-        Контакты
-        </a>
-      </li>
-
-
-     </ul>
   );
 }
 
